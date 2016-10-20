bc6376a9aa03b54f95f8b65cb0fbc30744aa5d8b
Aleksandar Damjanovic <codejanovic@users.noreply.github.com>
2016-10-20 14:37:52 +0200

    initial commit

---
+++ src/test/java/io/github/katas/guildedrose/GildedRoseTest.java
@@ -0,0 +1,18 @@
+package io.github.katas.guildedrose;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+}
